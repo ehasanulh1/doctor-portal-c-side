f96d97e95c98c915b98769a0ac8b16b7318a2b85
ehasanulh1 <ehasanulh1@gmail.com>
2024-01-03 21:23:04 +0600

    create folder and set layout with router

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Footer = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            this is footer
         </div>
     );
 };
 
-export default Home;
+export default Footer;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Navbar = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <h2>This in nav</h2>
         </div>
     );
 };
 
-export default Home;
+export default Navbar;
