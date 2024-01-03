29cc7caf160024996f15841d45d1a38e79d76676
ehasanulh1 <ehasanulh1@gmail.com>
2024-01-03 20:41:43 +0600

    install some extentions for new vs code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-5xl tracking-widest	 font-bold underline">
-      Hello world!
-    </h1>
-    <button
-  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
-</button>
+    <div>
     </div>
   );
 }
