611911e646b8a7d315e12b9d8c1219ff8e1f3906
ehasanulh1 <ehasanulh1@gmail.com>
2024-01-04 19:38:30 +0600

    start service section

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='mt-20 pt-10'>
+            <div className='text-center'>
+                <h3 className='text-primary font-bold text-xl uppercase'>OUR SERVICES</h3>
+                <h2 className='text-3xl'>Services We Provide</h2>
+            </div>
+            <div>
+            
+            </div>
+        </div>
+    );
+};
+
+export default Services;
